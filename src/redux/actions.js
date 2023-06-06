a0287ce4669039bd72a8f54a138e36754ca5c89b
nicobonder <nicobonder@hotmail.com>
2023-06-06 17:42:38 -0300

    Starting to deploy in railway

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -21,7 +21,8 @@ export const getPokemons = () => {
     return function(dispatch) {
         //return fetch('https://pokemonapi-jzai.onrender.com/pokemons')
         
-        return fetch('http://localhost:3001/pokemons')
+        //return fetch('http://localhost:3001/pokemons')
+        return fetch('http://pokemonback.up.railway.app/pokemons')
         .then(res => res.json())
         .then(pokemons => dispatch(
             {type: GET_POKEMONS, payload: pokemons}
@@ -32,7 +33,9 @@ export const getPokemons = () => {
 export const getPokemonDetail = (id) => {
     return async function(dispatch) {
         //return fetch(`https://pokemonapi-jzai.onrender.com/pokemons/${id}`)    
-        return fetch(`http://localhost:3001/pokemons/${id}`)    
+        //return fetch(`http://localhost:3001/pokemons/${id}`)    
+        
+        return fetch(`http://pokemonback.up.railway.app/pokemons/${id}`)    
         .then(res => res.json())
         .then(data => dispatch(
             {type: GET_POKEMON_DETAIL, payload: data[0]}
@@ -44,7 +47,8 @@ export const searchPokemon = (name) => {
     return async function(dispatch) {
         try {
             //let info =  await axios.get("https://pokemonapi-jzai.onrender.com/pokemons?name=" + name);
-            let info =  await axios.get("http://localhost:3001/pokemons?name=" + name);
+            // let info =  await axios.get("http://localhost:3001/pokemons?name=" + name);
+            let info =  await axios.get("http://pokemonback.up.railway.app?name=" + name);
             return dispatch({
                 type: "SEARCH_POKEMON",
                 payload: info.data
@@ -57,7 +61,9 @@ export const searchPokemon = (name) => {
 
 export const createPokemon = (pokemon) => {
     return async function(dispatch){
-        const newPokemon = await axios.post(`http://localhost:3001/pokemons/`, pokemon)
+        //const newPokemon = await axios.post(`http://localhost:3001/pokemons/`, pokemon)
+
+        const newPokemon = await axios.post(`http://pokemonback.up.railway.app/pokemons/`, pokemon)
         //const newPokemon = await axios.post(`https://pokemonapi-jzai.onrender.com/pokemons/`, pokemon)
        dispatch({type: CREATE_POKEMON, payload: newPokemon})
     }
@@ -65,7 +71,9 @@ export const createPokemon = (pokemon) => {
 
 export const updatePokemon = (updatePoke) => {
     return async function(dispatch){ //va la ruta del poke que tiene que actualizar
-        await axios.put(`http://localhost:3001/pokemons/${updatePoke.id}`, updatePoke)
+        //await axios.put(`http://localhost:3001/pokemons/${updatePoke.id}`, updatePoke)
+
+        await axios.put(`http://pokemonback.up.railway.app/pokemons/${updatePoke.id}`, updatePoke)
         //await axios.put(`https://pokemonapi-jzai.onrender.com/pokemons/${updatePoke.id}`, updatePoke)
         .then((res) => { 
             dispatch({type: UPDATE_POKEMON, payload: res.data}) //res.data da OK
@@ -78,7 +86,9 @@ export const updatePokemon = (updatePoke) => {
 
 export const deletePokemon = (pokemonId) => {
     return async function(dispatch){
-       const deletePoke = await axios.delete(`http://localhost:3001/pokemons/${pokemonId}`, pokemonId )
+       //const deletePoke = await axios.delete(`http://localhost:3001/pokemons/${pokemonId}`, pokemonId )
+       const deletePoke = await axios.delete(`http://pokemonback.up.railway.app/pokemons/${pokemonId}`, pokemonId )
+       
        // const deletePoke = await axios.delete(`https://pokemonapi-jzai.onrender.com/pokemons/${pokemonId}`, pokemonId )
         dispatch({type: DELETE_POKEMON, payload: deletePoke})
     }
@@ -86,7 +96,9 @@ export const deletePokemon = (pokemonId) => {
 
 export const getTypes = () => {
     return async function(dispatch){
-        let info = await axios.get('http://localhost:3001/types', {})
+        //let info = await axios.get('http://localhost:3001/types', {})
+
+        let info = await axios.get('http://pokemonback.up.railway.app/types', {})
         //let info = await axios.get('https://pokemonapi-jzai.onrender.com/types', {})
         return dispatch({type: GET_TYPES, payload: info.data})
     }
